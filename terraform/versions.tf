d33aee9ce338bfdf2a2cfaabbd35a4798ec12426
jingood2 <jingood2@gmail.com>
2023-04-26 02:08:47 +0900

    Initial Commit

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,11 +1,21 @@
 terraform {
-  required_version = ">= 0.13.1"
+  required_version = ">= 1.0.1"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.73"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.1"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.8.0"
+    }
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
+
   }
 
   backend "s3" {}
