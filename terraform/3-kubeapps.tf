60e5c54e4576a4ec82dfb7f39797cc3d9f5539ce
jingood2 <jingood2@gmail.com>
2023-04-26 16:55:50 +0900

    add helm kubeapps

---
+++ terraform/3-kubeapps.tf
@@ -0,0 +1,58 @@
+# helm repo add bitnami https://charts.bitnami.com/bitnami
+# helm repo update
+# helm install argocd -n argocd --create-namespace argo/argo-cd --version 3.35.4 -f terraform/values/argocd.yaml
+# kubectl get --namespace default secret kubeapps-operator-token -o go-template='{{.data.token | base64decode}}'
+resource "helm_release" "kubeapps" {
+  name        = "kubeapps"
+  repository  = "https://charts.bitnami.com/bitnami"
+  chart       = "kubeapps"
+  namespace   = "kubeapps"
+  create_namespace = true
+  version     = "12.3.1"
+
+  values = [
+    "${file("values/kubeapps.yaml")}"
+  ]
+}
+
+resource "kubectl" "serviceaccount" {
+  yaml_body = <<YAML
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: kubeapps-operator 
+  namespace: default
+  resourceVersion: "340"
+  uid: 63bd3d9e-6caa-463c-b23e-be99126b683f
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  annotations:
+    rbac.authorization.kubernetes.io/autoupdate: "true"
+  labels:
+    kubernetes.io/bootstrapping: rbac-defaults
+  name: kubeapps-operator
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- apiGroup: rbac.authorization.k8s.io
+  kind: Group
+  name: default:kubeapps-operator
+  YAML
+}
+
+resource "kubectl_manifest" "kubeapps-operator-token" {
+    yaml_body = <<YAML
+apiVersion: v1
+kind: Secret
+metadata:
+  name: kubeapps-operator-token
+  namespace: default
+  annotations:
+    kubernetes.io/service-account.name: kubeapps-operator
+type: kubernetes.io/service-account-token
+YAML
+}
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -44,6 +44,18 @@ provider "helm" {
   }
 }
 
+provider "kubectl" {
+  apply_retry_count      = 10
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_id]
+    command     = "aws"
+  }
+}
+
+
 
 
 #backend "remote" {
