1fcc81e532adb58b321d4d0e1adbd19e18904009
jingood2 <jingood2@gmail.com>
2023-04-26 14:43:13 +0900

    update

--- terraform/.env/dev/terraform.tfvars
+++ terraform/.env/dev/terraform.tfvars
@@ -1,3 +1,5 @@
 aws_region  = "us-east-1"
 environment = "dev"
 project     = "jingood2"
+
+cluster_id  = "jingood2-dev-eks"
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -25,6 +25,25 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider kubernetes {
+  host                   = element(concat(data.aws_eks_cluster.cluster[*].endpoint, list("")), 0)
+  cluster_ca_certificate = base64decode(element(concat(data.aws_eks_cluster.cluster[*].certificate_authority.0.data, list("")), 0))
+  token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, list("")), 0)
+  load_config_file       = false
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_id]
+      command     = "aws"
+    }
+  }
+}
+
 
 
 #backend "remote" {
