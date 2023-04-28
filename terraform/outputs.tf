6dcd0d5a8d1515c39c0e2e8399c4936cda9ee4ea
jingood2 <jingood2@gmail.com>
2023-04-29 02:18:41 +0900

    update output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,12 +5,12 @@ output "kubeapps_manifest" {
 
 output "argocd_manifest" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = var.enable_arogcd ? helm_release.arogcd[0].manifest : null
+  value       = var.enable_argocd? helm_release.argocd[0].manifest : null
 }
 
 output "image_updater_manifest" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = var.enable_image_updater ? helm_release.image_updater[0].manifest : null
+  value       = var.enable_image_updater ? helm_release.image-updater[0].manifest : null
 }
 
 output "chartmuseum_manifest" {
@@ -20,7 +20,7 @@ output "chartmuseum_manifest" {
 
 output "sealed_secrets_manifest" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = var.enable_sealed_secrets ? helm_release.sealed_secrets[0].manifest : null
+  value       = var.enable_sealed_secrets ? helm_release.sealed-secrets[0].manifest : null
 }
 
 output "traefik_manifest" {
