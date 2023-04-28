4434e6b75429e49e7cf59793d97e5e9f92354d37
jingood2 <jingood2@gmail.com>
2023-04-29 02:13:39 +0900

    update output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,29 +1,29 @@
 output "kubeapps_manifest" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = var.enable_kubeapps ? helm_release.kubeapps.manifest : null
+  value       = var.enable_kubeapps ? helm_release.kubeapps[0].manifest : null
 }
 
 output "argocd_manifest" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = var.enable_arogcd ? helm_release.arogcd.manifest : null
+  value       = var.enable_arogcd ? helm_release.arogcd[0].manifest : null
 }
 
 output "image_updater_manifest" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = var.enable_image_updater ? helm_release.image_updater.manifest : null
+  value       = var.enable_image_updater ? helm_release.image_updater[0].manifest : null
 }
 
 output "chartmuseum_manifest" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = var.enable_chartmuseum ? helm_release.chartmuseum.manifest : null
+  value       = var.enable_chartmuseum ? helm_release.chartmuseum[0].manifest : null
 }
 
 output "sealed_secrets_manifest" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = var.enable_sealed_secrets ? helm_release.sealed_secrets.manifest : null
+  value       = var.enable_sealed_secrets ? helm_release.sealed_secrets[0].manifest : null
 }
 
 output "traefik_manifest" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = var.enable_traefik ? helm_release.traefik.manifest : null
+  value       = var.enable_traefik ? helm_release.traefik[0].manifest : null
 }
