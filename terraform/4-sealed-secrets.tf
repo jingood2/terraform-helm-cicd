7e2cd6f04a9a986dda96c6c6247b391c4b5a7ad8
jingood2 <jingood2@gmail.com>
2023-05-06 03:40:57 +0900

    update sealed secret controller

--- terraform/4-sealed-secrets.tf
+++ terraform/4-sealed-secrets.tf
@@ -1,15 +1,14 @@
-# helm repo add argo https://chartmuseum.github.io/charts
+# helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 # helm repo update
-# helm install chartmuseum -n chartmuseum --create-namespace chartmuseum/chartmuseum --version 3.9.3 -f terraform/values/chartmuseum.yaml
+# helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
 resource "helm_release" "sealed-secrets" {
   count = var.enable_sealed_secrets ? 1 : 0
 
-  name             = "${local.prefix_name}-sealed-secrets"
-  repository       = "https://charts.bitnami.com/bitnami"
+  name             = "sealed-secrets-controller"
+  repository       = "https://bitnami-labs.github.io/sealed-secrets"
   chart            = "sealed-secrets"
   namespace        = "kube-system"
-  create_namespace = true
-  version          = "1.2.15"
+  version          = "2.8.2"
   values = [
     "${file("values/sealed-secrets.yaml")}"
   ]
