1f407a6810297c01b058812e8ae7e09b90a9a44b
jingood2 <jingood2@gmail.com>
2023-05-08 07:23:03 +0900

    backstage image v0.6.0

--- terraform/6-backstage.tf
+++ terraform/6-backstage.tf
@@ -169,7 +169,7 @@ spec:
     spec:
       containers:
         - name: backstage
-          image: jingood2/backstage:v0.5.0
+          image: jingood2/backstage:v0.6.0
           imagePullPolicy: IfNotPresent
           ports:
             - name: http
